630d7a346d83a52d81adae0dd354f371cdd8eca3
thientu745 <thientu745@gmail.com>
2025-11-15 13:50:05 -0800

    creates home, profile, search skeleton tabs

---
+++ HikaApp/app/(tabs)/_layout.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Tabs } from 'expo-router'
+import React from 'react'
+import {Ionicons} from "@expo/vector-icons"
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+        screenOptions={{
+            tabBarActiveTintColor: "green",
+            tabBarInactiveTintColor: "grey",
+        }}
+    >
+        <Tabs.Screen
+            name='home'
+            options={{
+            title: "Home",
+            tabBarIcon: ({ color, size }) => (
+                <Ionicons name="home-outline" size={size} color={color} />
+            ),
+            }}
+        />
+        <Tabs.Screen
+            name='search'
+            options={{
+            title: "Search",
+            tabBarIcon: ({ color, size }) => (
+                <Ionicons name="search-outline" size={size} color={color} />
+            ),
+            }}
+        />
+        <Tabs.Screen
+            name='profile'
+            options={{
+            title: "Profile",
+            tabBarIcon: ({ color, size }) => (
+                <Ionicons name="person-outline" size={size} color={color} />
+            ),
+            }}
+        />
+    </Tabs>
+  )
+}
+
+export default TabsLayout
